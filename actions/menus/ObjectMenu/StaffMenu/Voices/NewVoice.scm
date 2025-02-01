5f977dc2ccb9c3abd4c7ad75ad0d1324be27a27c
Richard Shann <richard@rshann.plus.com>
2025-02-01 17:54:08 +0000

    Revert "Duplicate any key changes in NewVoice"
    This reverts commit 5a0365a804324f9e86697b832912c167e6ea6462.

--- actions/menus/ObjectMenu/StaffMenu/Voices/NewVoice.scm
+++ actions/menus/ObjectMenu/StaffMenu/Voices/NewVoice.scm
@@ -1,13 +1,3 @@
 ;;NewVoice
 (d-NewStructuredStaff 'voice)
 (d-InheritStaffProperties)
-(d-MoveToStaffUp)
-(d-GoToBeginning)
-(while (d-NextObject)
-	(if (Keysignature?)
-		(begin
-			(d-PushPosition)
-			(d-SetMark)(d-Copy)
-			(d-MoveToStaffDown)
-			(d-Paste)
-			(d-PopPosition))))
