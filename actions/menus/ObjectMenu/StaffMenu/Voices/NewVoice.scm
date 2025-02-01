5a0365a804324f9e86697b832912c167e6ea6462
Richard Shann <richard@rshann.plus.com>
2025-02-01 16:34:48 +0000

    Duplicate any key changes in NewVoice

--- actions/menus/ObjectMenu/StaffMenu/Voices/NewVoice.scm
+++ actions/menus/ObjectMenu/StaffMenu/Voices/NewVoice.scm
@@ -1,3 +1,13 @@
 ;;NewVoice
 (d-NewStructuredStaff 'voice)
 (d-InheritStaffProperties)
+(d-MoveToStaffUp)
+(d-GoToBeginning)
+(while (d-NextObject)
+	(if (Keysignature?)
+		(begin
+			(d-PushPosition)
+			(d-SetMark)(d-Copy)
+			(d-MoveToStaffDown)
+			(d-Paste)
+			(d-PopPosition))))
