3758c3e5135da12048eb69c604ca54b10f213273
Richard Shann <richard@rshann.plus.com>
2025-06-25 11:27:46 +0100

    Allow for earlier LilyPond versions

--- actions/menus/ObjectMenu/Directives/Barlines/RepeatStart.scm
+++ actions/menus/ObjectMenu/Directives/Barlines/RepeatStart.scm
@@ -1,2 +1,4 @@
 ;;;RepeatStart
-(StandAloneSelfEditDirective (cons "RepeatStart" "\\bar \".|:-|\"") #t "RepeatStart")
+(if (d-CheckLilyVersion "2.20.0")
+	(StandAloneSelfEditDirective (cons "RepeatStart" "\\bar \".|:-|\"") #t "RepeatStart")
+	(StandAloneSelfEditDirective (cons "RepeatStart" "\\bar \".|:\"") #t "RepeatStart"))
