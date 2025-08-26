e711e6607e392c4ff254561d91cf520b4b526866
Richard Shann <richard@rshann.plus.com>
2025-08-26 14:21:19 +0100

    Copy subpart name when cloning staff

--- src/command/staff.c
+++ src/command/staff.c
@@ -160,6 +160,7 @@ void
 staff_copy (DenemoStaff * src, DenemoStaff * dest, gboolean copy_all)
 {
   dest->denemo_name = g_string_new (src->denemo_name->str);
+  if (src->subpart) dest->subpart = g_string_new (src->subpart->str);
   dest->lily_name = g_string_new (src->lily_name->str);
   dest->midi_instrument = g_string_new (src->midi_instrument->str);
   dest->midi_channel = src->midi_channel;
