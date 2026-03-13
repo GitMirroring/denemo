ff6c4bd58642948af130174daeac9d53f0105b95
Jeremiah Benham <jjbenham@chicagoguitar.com>
2026-03-13 14:58:17 +0000

    Remove obsolete gub and jhbuild build systems

--- gub/gub/specs/perl-xml-parser.py
+++
@@ -1,5 +0,0 @@
-from gub import tools
-
-class Perl_xml_parser (tools.CpanBuild):
-    source = 'http://search.cpan.org/CPAN/authors/id/M/MS/MSERGEANT/XML-Parser-2.34.tar.gz'
-    dependencies = ['expat']
