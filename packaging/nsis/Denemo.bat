d0890f41f760f2f350cc07740ef0d21b8617fc32
Jeremiah Benham <jjbenham@chicagoguitar.com>
2026-04-30 20:16:24 +0000

    Most of this stuff should only run the first time denemo is installed. No need to run it every time

--- packaging/nsis/Denemo.bat
+++ packaging/nsis/Denemo.bat
@@ -1,40 +1,34 @@
 @ECHO OFF
 cd /D %~dp0
-
 @echo off
 
-REM ------------------------------------------------------------------------------
-REM Fix Guile .go file timestamps on first run (or after unzip timestamp reset).
-REM Windows unzip sets all files to "now", making .scm appear newer than .go.
-REM Guile then tries to recompile .go files, which fails with:
-REM   "Wrong number of arguments to #<boot-closure>"
-REM We stamp .go files to a far-future date so Guile always trusts them.
-REM This is safe - Guile only checks relative age, not absolute date.
-REM ------------------------------------------------------------------------------
-
-SET "LILY_LIB=%~dp0lilypond\lib"
-
-REM Check if already fixed (sentinel file)
+REM Fix Guile .go file timestamps on first run
 IF NOT EXIST "%~dp0.go_timestamps_fixed" (
     echo Fixing Guile .go timestamps, please wait...
-    FOR /R "%LILY_LIB%" %%F IN (*.go) DO (
+    FOR /R "%~dp0lilypond\lib" %%F IN (*.go) DO (
         copy /b "%%F"+,, "%%F" >nul 2>&1
     )
     echo. > "%~dp0.go_timestamps_fixed"
     echo Done.
 )
 
-REM Regenerate pixbuf loaders cache
-if exist bin\gdk-pixbuf-query-loaders.exe (
-    bin\gdk-pixbuf-query-loaders.exe lib\gdk-pixbuf-2.0\2.10.0\loaders\*.dll > lib\gdk-pixbuf-2.0\2.10.0\loaders.cache 2>nul
+REM Regenerate pixbuf loaders cache once
+IF NOT EXIST "%~dp0.pixbuf_cache_built" (
+    if exist bin\gdk-pixbuf-query-loaders.exe (
+        bin\gdk-pixbuf-query-loaders.exe lib\gdk-pixbuf-2.0\2.10.0\loaders\*.dll > lib\gdk-pixbuf-2.0\2.10.0\loaders.cache 2>nul
+        echo. > "%~dp0.pixbuf_cache_built"
+    )
 )
 
-REM Regenerate GTK IM modules cache
-if exist bin\gtk-query-immodules-3.0.exe (
-    bin\gtk-query-immodules-3.0.exe lib\gtk-3.0\3.0.0\immodules\*.dll > lib\gtk-3.0\3.0.0\immodules.cache 2>nul
+REM Regenerate GTK IM modules cache once
+IF NOT EXIST "%~dp0.immodules_cache_built" (
+    if exist bin\gtk-query-immodules-3.0.exe (
+        bin\gtk-query-immodules-3.0.exe lib\gtk-3.0\3.0.0\immodules\*.dll > lib\gtk-3.0\3.0.0\immodules.cache 2>nul
+        echo. > "%~dp0.immodules_cache_built"
+    )
 )
 
-REM Compile GLib schemas if needed
+REM Compile GLib schemas once
 if not exist share\glib-2.0\schemas\gschemas.compiled (
     bin\glib-compile-schemas.exe share\glib-2.0\schemas
 )
@@ -43,7 +37,6 @@ REM Guile settings
 set GUILE_LOAD_PATH=%~dp0share\guile\2.2
 set GUILE_LOAD_COMPILED_PATH=%~dp0lib\guile\2.2\ccache
 set GUILE_AUTO_COMPILE=0
-
 
 REM GIO/GLib settings
 set GIO_USE_VFS=local
@@ -56,42 +49,45 @@ set FONTCONFIG_FILE=%~dp0etc\fonts\fonts.conf
 REM GTK settings
 set XDG_DATA_DIRS=%~dp0share
 set XDG_DATA_HOME=%~dp0share
-set GTK_DATA_PREFIX=%~dp065;11M65;11m
-
+set GTK_DATA_PREFIX=%~dp0
 set GTK_EXE_PREFIX=%~dp0
 
 REM Path
 set PATH=%~dp0bin;%PATH%
 
-REM already did this I think. cd /d "%~dp0"
+REM Font cache once
 set FONTCONFIG_PATH=%~dp0etc\fonts
 set FC_CACHEDIR=%~dp0fontconfig\cache
-
 if not exist fontconfig\cache mkdir fontconfig\cache
-
-if not exist fontconfig\cache\* (
+IF NOT EXIST "%~dp0.fontconfig_cache_built" (
     bin\fc-cache.exe -fv share\fonts
+    echo. > "%~dp0.fontconfig_cache_built"
 )
 
-bin\regfont.exe -a share\fonts\truetype\denemo\feta.ttf
-bin\regfont.exe -a share\fonts\truetype\denemo\Denemo.ttf
-bin\regfont.exe -a share\fonts\truetype\denemo\emmentaler.ttf
+REM Register fonts once
+IF NOT EXIST "%~dp0.fonts_registered" (
+    bin\regfont.exe -a share\fonts\truetype\denemo\feta.ttf
+    bin\regfont.exe -a share\fonts\truetype\denemo\Denemo.ttf
+    bin\regfont.exe -a share\fonts\truetype\denemo\emmentaler.ttf
+    echo. > "%~dp0.fonts_registered"
+)
+
+REM Mime database once
+IF NOT EXIST "%~dp0.mime_db_built" (
+    if exist bin\update-mime-database.exe bin\update-mime-database.exe share\mime
+    echo. > "%~dp0.mime_db_built"
+)
 
 set EVINCE_BACKENDS_DIR=%~dp0lib\evince\4\backends
 set GI_TYPELIB_PATH=%~dp0lib\girepository-1.0
-
-REM mime database
-if exist bin\update-mime-database.exe bin\update-mime-database.exe share\mime
-
+set DBUS_SESSION_BUS_ADDRESS=disabled
+set NO_AT_BRIDGE=1
 
 echo Environment ready, launching Denemo...
 echo GUILE_LOAD_PATH=%GUILE_LOAD_PATH%
 echo GUILE_LOAD_COMPILED_PATH=%GUILE_LOAD_COMPILED_PATH%
 
-set DBUS_SESSION_BUS_ADDRESS=disabled
-set NO_AT_BRIDGE=1
 bin\denemo.exe %*
-
 IF %ERRORLEVEL% NEQ 0 (
     echo Denemo exited with code %ERRORLEVEL%
     pause
